f90a21e9e1e6f53d211f845710c86a8bac3b1288
Sweezar <royal13flush@gmail.com>
2022-12-27 14:51:09 +0300

    -При отсутствии карточек событий предлагает добавить

--- src/main.js
+++ src/main.js
@@ -9,18 +9,10 @@ import SortView from './view/sort.js';
 import ListView from './view/trip-list.js';
 import EditFormView from './view/edit-event-form.js';
 import EventView from './view/event.js';
+import NoEventView from './view/no-event.js';
 
 const EVENTS_COUNT = 4;
 const events = new Array(EVENTS_COUNT).fill().map(generatePoint);
-
-const tripMain = document.querySelector('.trip-main');
-const InfoContainer = new InfoContainerView();
-render(tripMain, InfoContainer, RenderPosition.AFTERBEGIN);
-
-const infoMain = new InfoMainView(events);
-const cost = new CostView(events);
-render(InfoContainer, infoMain, RenderPosition.BEFOREEND);
-render(InfoContainer, cost, RenderPosition.BEFOREEND);
 
 const navigation = document.querySelector('.trip-controls__navigation');
 const tabs = new TabsView();
@@ -28,16 +20,30 @@ render(navigation, tabs, RenderPosition.BEFOREEND);
 
 const filter = new FilterView();
 render(navigation, filter, RenderPosition.BEFOREEND);
-
 const tripEventsContainer = document.querySelector('.trip-events');
-const sort = new SortView();
-render(tripEventsContainer, sort, RenderPosition.BEFOREEND);
 
-const eventsList = new ListView();
-render(tripEventsContainer, eventsList, RenderPosition.BEFOREEND);
+if (events.length == 0) {
+  const noEvent = new NoEventView();
+  render(tripEventsContainer, noEvent, RenderPosition.BEFOREEND);
+} else {
+  const tripMain = document.querySelector('.trip-main');
+  const InfoContainer = new InfoContainerView();
+  render(tripMain, InfoContainer, RenderPosition.AFTERBEGIN);
 
-for (let i = 0; i < EVENTS_COUNT; i++) {
-  renderEvent(eventsList, events[i]);
+  const sort = new SortView();
+  render(tripEventsContainer, sort, RenderPosition.BEFOREEND);
+
+  const infoMain = new InfoMainView(events);
+  const cost = new CostView(events);
+  render(InfoContainer, infoMain, RenderPosition.BEFOREEND);
+  render(InfoContainer, cost, RenderPosition.BEFOREEND);
+
+  const eventsList = new ListView();
+  render(tripEventsContainer, eventsList, RenderPosition.BEFOREEND);
+
+  for (let i = 0; i < EVENTS_COUNT; i++) {
+    renderEvent(eventsList, events[i]);
+  }
 }
 
 function renderEvent(eventListElement, event) {
